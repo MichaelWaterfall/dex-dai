1ed2f8cb696d4413ba13933e8c3fc7e8464af2ad
Michael Waterfall <michaeljames.waterfall@gmail.com>
2022-10-14 09:09:20 +0100

    first commit

---
+++ client/src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-center">
+        Michael Waterfall 2022
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
